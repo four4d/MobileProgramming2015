18f45dfc1a621ef1b8a5f4813aebc3f5c74da023
JinSung Kim <four4d@naver.com>
2015-10-08 05:16:07 +0900

    5주차_3,4번문제

--- src/kr/ac/embedded/kookmin/callback/CallbackTest.java
+++ src/kr/ac/embedded/kookmin/callback/CallbackTest.java
@@ -20,10 +20,13 @@ public class CallbackTest implements OnMaxNumberCb {
         System.out.println("Total is" + total.getTotal());
     }
 
+    //OnMaxNumberCb 인터페이스의 onMaxNumber 메소드 오버라이드
+    //최대값을 초과했을 경우 그 때의 현재값과 초과값을 출력.
     @Override
     public void onMaxNumber(int number, int exceed) {
         // TODO Auto-generated method stub
         // 구현하시오 //
+        System.out.println("Current sum is " + number + " and exceeds " + exceed);
     }
     
 
---
+++ src/kr/ac/kookmin/embedded/overriding/Monster.java
@@ -0,0 +1,8 @@
+package kr.ac.kookmin.embedded.overriding;
+
+class Monster {
+    boolean frighten(int d) {
+        System.out.println("arrrgh");
+        return true;
+    }
+}
