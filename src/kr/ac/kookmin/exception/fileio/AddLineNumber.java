1ac7ddcd9e65dd806b481788e37469fa45923466
JinSung Kim <four4d@naver.com>
2015-10-08 14:34:23 +0900

    6주차 3번문제

--- src/kr/ac/kookmin/exception/fileio/AddLineNumber.java
+++ src/kr/ac/kookmin/exception/fileio/AddLineNumber.java
@@ -3,7 +3,6 @@ package kr.ac.kookmin.exception.fileio;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.io.Reader;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -29,11 +28,11 @@ public class AddLineNumber
          String str;
          int lineNumber = 0;
          
-         while((str = inputStream.readLine()) != null){
+         while((str = inputStream.readLine()) != null){ //inputStream에서 한 줄씩 읽음. null일 경우 반복문 종료.
              lineNumber++;
              str = Integer.toString(lineNumber).concat(" ").concat(str).concat("\n");
-             System.out.println(str);
-             outputStream.write(str);             
+             System.out.println(str);  //모니터링을 위한 콘솔 출력.
+             outputStream.write(str);  //파일에 한 줄 출력.             
          }
          
           /**
@@ -42,7 +41,7 @@ public class AddLineNumber
          inputStream.close( );
          outputStream.close( );
       } catch(Exception e) {
-          System.out.println("error발생");
+          System.out.println("error발생 : origianl.txt파일이 존재하는지 확인해주십시오.");
       }
       
       /** catch() 구문 작성하시오 **/
